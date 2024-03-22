6f0c60d26700383b0a61ed12c79cb7b2ad515148
Bhavin karena <bhavinkarena2003@gmail.com>
2024-03-22 16:53:38 +0530

    forbricc

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["Authorization", "Content-Type"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(user.app)
